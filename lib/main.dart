7b68ebb9cada7a885d497c7d1f8442e361733d27
Dmytro Batarin <dmytro.jr@batarin.zp.ua>
2020-05-13 07:58:24 +0300

    Added base ui elements for quiz

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal'
+    ];
+
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(title: Text("My first app")),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(onPressed: null, child: Text('Answer 1')),
+            RaisedButton(onPressed: null, child: Text('Answer 2')),
+            RaisedButton(onPressed: null, child: Text('Answer 3')),
+          ],
+        ),
+      ),
     );
   }
 }
